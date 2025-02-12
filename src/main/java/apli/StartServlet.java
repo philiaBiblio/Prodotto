887bf628b7f05f41dd158b4e4d592f18800ee067
Alice <atwjpejnh2751tgrs@icloud.com>
2025-02-12 10:47:22 +0900

    スタートサーブレットの作成

---
+++ src/main/java/apli/StartServlet.java
@@ -0,0 +1,34 @@
+package apli;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class StartServlet
+ */
+@WebServlet("/index.html")
+public class StartServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		// ログイン画面へ
+		String url = "P2Login.jsp";
+		System.out.println(url);
+					
+		// 画面遷移
+		RequestDispatcher rd = request.getRequestDispatcher(url);
+		rd.forward(request, response);
+	}
+
+}
